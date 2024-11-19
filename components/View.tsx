8ae3b6a2abf94b69b9008ebe9bfac4bcb6f2e2fc
Daniel Choi <danielchoi@dhcp-vl2051-1755.eduroam.cornell.edu>
2024-11-19 15:08:29 -0500

    views updated with partial pre-rendering

--- components/View.tsx
+++ components/View.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Ping from './Ping'
 import { client } from '@/sanity/lib/client'
 import { STARTUP_VIEWS_QUERY } from '@/sanity/lib/queries'
+import { writeClient } from '@/sanity/lib/write-client'
+import { unstable_after as after} from 'next/server'
 
 const View = async ({ id }: { id: string }) => {
 
@@ -9,7 +11,12 @@ const View = async ({ id }: { id: string }) => {
         useCdn: false
     }).fetch(STARTUP_VIEWS_QUERY, {id})
 
-    // TODO: Update the number of views
+    after(async () => await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+    )
+
 
     return (
         <div className='view-container'>
